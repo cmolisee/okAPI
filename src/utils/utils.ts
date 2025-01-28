b5c878f17ae9001d836169963764699733be704b
Cody.Molisee <cody.molisee@creditone.com>
2025-01-28 13:55:42 -0800

    chore: clean project in prep for refactor.

--- src/utils/utils.ts
+++
@@ -1,5 +0,0 @@
-export const MS_TO_HR_FACTOR: number = 2777777;
-
-export function convertMsToHr(ms: number) {
-	return ms / MS_TO_HR_FACTOR;
-}
--- webpack.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Configuration } from 'webpack';
-
-module.exports = {
-    entry: { 
-        background: 'src/chromeServices/backgroundScript.ts',
-        contentCommunication: 'src/chromeServices/contentScriptCommunication.ts'
-    },
-} as Configuration;
