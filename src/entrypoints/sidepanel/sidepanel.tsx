cb11644695b8ddc99011b33ff8ed9d3974ec148b
Cody.Molisee <cody.molisee@creditone.com>
2025-02-03 07:40:35 -0800

    chore: cleanup and optimization

--- src/entrypoints/sidepanel/sidepanel.tsx
+++ src/entrypoints/sidepanel/sidepanel.tsx
@@ -16,20 +16,22 @@ function Sidepanel() {
   })
 
   return (
-    <>
-      <div>
+    <div class="grid grid-cols-10 grid-rows-1 gap-1">
+      <div class="col-span-2">Side Panel</div>
+      <div class="col-span-8">
+        <p>main content</p>
+        <h1>okAPI</h1>
+        <div class="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count()}
+          </button>
+          <button onClick={async () => await sendMessageUpdate(count()).catch(e => console.debug)}>
+            send count to webpage
+          </button>
+          <a href='/options'>to the options page</a>
+        </div>
       </div>
-      <h1>okAPI</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <button onClick={async () => await sendMessageUpdate(count()).catch(e => console.debug)}>
-          send count to webpage
-        </button>
-        <a href='/options'>to the options page</a>
-      </div>
-    </>
+    </div>
   );
 }
 
